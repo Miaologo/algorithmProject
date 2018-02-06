2edef0f354ea2f11a550f8d4b9c6b2118975904a
liumiao <liumiao05@meituan.com>
2018-02-06 15:51:12 +0800

    20180206

---
+++ LMAglorithm.playground/Pages/AVLTree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+func llRotate(_ rootNode: TreeNode) -> TreeNode {
+    
+    
+    return TreeNode(0)
+}
--- LMAglorithm.playground/Sources/StringExtension.swift
+++ LMAglorithm.playground/Sources/StringExtension.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 extension String {
-//    subscript (bounds: CountableClosedRange<Int>) -> String {
-//        let start = index(startIndex, offsetBy: bounds.lowerBound)
-//        let end = index(startIndex, offsetBy: bounds.upperBound)
-//        return String(self[start...end])
-//    }
-//    subscript (bounds: CountableRange<Int>) -> String {
-//        let start = index(startIndex, offsetBy: bounds.lowerBound)
-//        let end = index(startIndex, offsetBy: bounds.upperBound)
-//        return String(self[start..<end])
-//    }
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
 }
