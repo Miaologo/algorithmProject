b036fb00307a4feb3f7e51b0a165e855ecb9b0b3
liumiao <liumiao05@meituan.com>
2018-05-15 18:36:48 +0800

    update 2018-05-15

--- ExcelSheetColumnNumber/ExcelSheetColumnNumber.cpp
+++ ExcelSheetColumnNumber/ExcelSheetColumnNumber.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 int titleToNumber(string s) {
-	
-	return 0;		
+	int result = 0;
+	for (int i = 0;i < s.length();i+=1) {
+		char x = s.at(i);
+		result = result * 26 + int(x) - 64;
+	}
+	return result;		
 }
 
 int main(int argc, char *argv[]) {
-
+	char a = 'A';
+	char z = 'Z';
+	cout << titleToNumber("AB") << endl;
 	return 0;
 }
